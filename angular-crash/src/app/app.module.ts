08831d5b5617ea89080a06c4c521316df1584f1c
lukeyzhong <lukeyzhong@gmail.com>
2021-11-28 21:59:23 -0600

    update3

--- angular-crash/src/app/app.module.ts
+++ angular-crash/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,7 +9,8 @@ import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+
 
 @NgModule({
   declarations: [
@@ -15,12 +18,14 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     HeaderComponent,
     ButtonComponent,
     TasksComponent,
-    TaskItemComponent
+    TaskItemComponent,
+    AddTaskComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-crash/src/app/components/tasks/tasks.component.ts
+++ angular-crash/src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TaskService } from 'src/app/services/task.service';
-import {Task} from '../../Task';
+import { Task } from '../../Task';
 
 @Component({
   selector: 'app-tasks',
@@ -12,8 +12,18 @@ export class TasksComponent implements OnInit {
 
   constructor(private taskService: TaskService) { }
 
-  ngOnInit(): void { 
-    this.taskService.getTasks().subscribe((tasks) =>this.tasks = tasks );
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => this.tasks = tasks);
   }
 
+  deleteTask(task: Task) {
+    this.taskService.deleteTask(task).subscribe(() =>
+      (this.tasks = this.tasks.filter(t => t.id !== task.id)));
+  }
+
+  toggleReminder(task: Task) {
+    task.reminder = !task.reminder;
+    this.taskService.updateTaskReminder(task).subscribe();
+  }
+  
 }
