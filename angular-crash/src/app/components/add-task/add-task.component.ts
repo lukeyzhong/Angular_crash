e74a5d0b38c99dd887591a38da49ec94ed32c197
lukeyzhong <lukeyzhong@gmail.com>
2021-12-02 01:10:22 -0600

    update2

--- angular-crash/src/app/components/add-task/add-task.component.ts
+++ angular-crash/src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import {Task} from '../../Task';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-task',
@@ -6,10 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-task.component.css']
 })
 export class AddTaskComponent implements OnInit {
+  text!: string;
+  day!: string;
+  reminder: boolean = false;
+  showAddTask!: boolean;
+  subscription: Subscription;
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
 
-  constructor() { }
+  constructor(private uiService: UiService) { 
+    this.subscription = this.uiService.onToggle()
+    .subscribe(value => (this.showAddTask = true));
+  }
 
   ngOnInit(): void {
+  }
+  
+  onSubmit() {
+    if (!this.text) {
+      alert('Please add a task!');
+      return;
+    }
+    
+    const newTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder
+    }
+    // emit the above task object
+    this.onAddTask.emit(newTask);
+
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
+
   }
 
 }
